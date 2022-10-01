27fb8bd821bc1434b21e29e159ccf3e2f0d7d374
Luciekinyanjui <lucy.wambui@moringaschool.com>
2022-10-01 11:45:13 +0300

    a change

---
+++ build/precache-manifest.c366097550df78a180e24b410ca4447a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "575569302296bf5c174f46e3e7d56e06",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d80a5b7eef491c4f44a4",
+    "url": "/static/js/2.49622c1b.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/static/js/2.49622c1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41d1cf62c78beb0f5ab4",
+    "url": "/static/js/main.e15bae26.chunk.js"
+  },
+  {
+    "revision": "3d8512d6fad6b06e930e",
+    "url": "/static/js/runtime-main.d2c1c694.js"
+  }
+]);
